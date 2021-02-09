c6c5be269a88de525e59fbf7e88968e479314acf
alessandro <finialessandro90@gmail.com>
2021-02-09 16:35:57 +0100

    cd filtrati per genere

--- booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
+++ booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
@@ -2,15 +2,39 @@ let app = new Vue ({
   el: '#app',
   data: {
     cdArray: '',
+    genreArray: ['All'],
+    selectedGenre: 'All'
   },
   mounted(){
     axios
       .get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((result) => {
+        /* array completo */
         this.cdArray = result.data.response;
+        console.log(this.cdArray);
+        /* array generi */
+        this.cdArray.forEach((element) => {
+          if (!this.genreArray.includes(element.genre)) {
+            this.genreArray.push(element.genre);
+            console.log(this.genreArray);
+          }
+          /* inserimento proprietà visible in oggetti cdArray */
+          element.visible = true;
+        });
+
+        console.log(this.selectedGenre);
       })
       .catch((error) => {
         console.log(error);
       });
+  },
+  methods: {
+    genreFilter: function(option){
+      this.cdArray.forEach((element) => {
+        (element.genre == option) ? element.visible = true
+        : (option == 'All') ? element.visible = true
+        : element.visible = false;
+      });
+    }
   }
 });
